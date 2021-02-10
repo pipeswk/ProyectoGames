ce4c3966d4a5f652eeeea2785142eda16d327b85
pipeswk <afco1995@gmail.com>
2021-02-09 21:10:23 -0500

    Cambios - Agregamos archivo if.js

---
+++ js/If.js
@@ -0,0 +1,24 @@
+// ================ En este archivo podras encontrar la manera en como se usa la condicional IF ================== //
+
+if () {
+                // Esta es la estructura que debe tener una condicional if
+}
+
+if (false) {  // Dentro de los () ponemos lo condicionales 
+    console.log("Hola");  // En este caso se imprime un Hola en caso de ser verdadero
+} else {     //El else se ocupa en caso de que el if no sea verdadero
+    console.log("Chao");
+}
+
+// ===================== Ejemplo con else if ==========================
+
+var edad = 18;
+if (edad === 18) {
+    console.log("puedes votar, sera tu primera votación :D");
+} else if (edad > 18) {
+    console.log("Puedes votar de nuevo");
+} else {
+    console.log("Aun no puedes votar :(")
+}
+
+
