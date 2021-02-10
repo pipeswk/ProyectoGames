464443f0eb69581813d05b0a3d67bc18fa47d009
pipeswk <afco1995@gmail.com>
2021-02-09 23:09:50 -0500

    Cambios - Se agrega juego de piedra papel y tijera

--- js/If.js
+++ js/If.js
@@ -15,3 +15,37 @@ if (edad === 18) {
 } else {
     console.log("Aun no puedes votar :(")
 }
+
+
+// ============ Podemos simplifircar el uso de los else if con condiciones, por ejemplo:
+
+var numero = 1;
+
+var resultado = numero === 1 ? "Si! Soy un uno :D" : "No! No soy un uno :(";
+
+
+// ======================= Juego de Piedra, papel o tijera =====================================
+
+var op1 = "Piedra";
+var op2 = "Papel";
+var op3 = "Tijera";
+
+var resultado = function(Player, CPU) {
+    if (Player === CPU) {
+        console.log("Esto es un empate !")
+    } else if (Player != CPU) {
+        if (Player === op1 && CPU === op3) {
+            console.log("Player es el ganador! :D")
+        }
+        if (Player === op2 && CPU === op1) {
+            console.log("Player es el ganador! :D")
+        }
+        if (Player === op3 && CPU == op2) {
+            console.log("Player es el ganador! :D")
+        }
+        else {
+            console.log("La CPU ha ganado :´(")
+        }
+    }
+};
+
